cb88ca98a1074668a0aed1ac5d93c47b62c6e2bc
alunno <alunno@lab03-06>
2017-02-02 10:34:59 +0100

    aggiunto file in c che somma un numero di 3 cifre da completare

---
+++ somma_cifre.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+ 
+int num;
+
+
+  printf("Digita un numero di 3 cifre ");
+  scanf("%d", &num);
+  
+if (num < 100 || num > 999) return -1;  
+   
+else {
+
+  
+
+
+  return 0;
+}
