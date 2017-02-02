c9b0f1cf9be2338746396624cbf3c9c847132c2c
alunno <alunno@lab03-06>
2017-02-02 09:27:13 +0100

    aggiunto resto

--- operazioni.c
+++ operazioni.c
@@ -15,12 +15,13 @@ int somma=a+b;
 int differenza=a-b;
 int prodotto=a*b;
 double divisione=a/b;
+int resto=a%b;
 
 printf("La somma di a e b e' %d\n", somma);
 printf("La differenza e' %d\n", differenza);
 printf("Il prodotto e' %d\n", prodotto);
 printf("Il quoziente e' %f\n", divisione);
-
+printf("Il resto della divisione e' %d\n", resto);
 
   return 0;
 }
