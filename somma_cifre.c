a5d09ccdaa48abc1ce8f2554f9fdeb5ad770229d
alunno <alunno@lab03-01>
2017-02-03 09:43:35 +0100

    somma cifre completata

--- somma_cifre.c
+++ somma_cifre.c
@@ -8,9 +8,26 @@ int num;
   printf("Digita un numero di 3 cifre ");
   scanf("%d", &num);
   
-if (num < 100 || num > 999) return -1;  
+if (num < 100 || num>999) printf("ERRORE\n");  
    
 else {
+
+ int c=num%10;
+ num = num/10;
+ int b=num%10;
+ num = num/10;
+ int a=num%10;
+
+   
+   
+   /*int a=num/100;
+    *int b=(num-(a*100))/10;
+    *int c=(num-(a*100))-b*10;
+    */
+  
+   int somma=a+b+c;
+
+   printf("La somma delle 3 cifre e' %d\n", somma);
 
 }  
 
